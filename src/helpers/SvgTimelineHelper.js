bc33e865e044e40a92c510b8725382cb5e165f4f
jniac <joseph.merdrignac@gmail.com>
2018-11-19 12:39:36 +0100

    on update

--- src/helpers/SvgTimelineHelper.js
+++ src/helpers/SvgTimelineHelper.js
@@ -1,7 +1,9 @@
 
 const makeSvg = (elementOrType, { parent, ...props }) => {
 
-    let element = elementOrType === 'object' ? elementOrType : document.createElementNS('http://www.w3.org/2000/svg', elementOrType)
+    let element = typeof elementOrType === 'object'
+        ? elementOrType
+        : document.createElementNS('http://www.w3.org/2000/svg', elementOrType)
 
     for (let [key, value] of Object.entries(props)) {
 
@@ -21,24 +23,28 @@ const makeSvg = (elementOrType, { parent, ...props }) => {
 
 const drawDivision = (svg, division, offsetY = 0) => {
 
-    let transform = `translate(${division.range.position * .5}, 0)`
-    let x2 = division.range.width * .5
+    let position = division.range.position * .5
+    let width = division.range.width * .5
     let y = 10 * division.ancestors.length + offsetY
 
-    let g = makeSvg('g', { parent:svg, stroke:division.props.color, 'stroke-width':3, transform })
+    let g = makeSvg('g', { parent:svg, stroke:division.props.color, 'stroke-width':3, transform:`translate(${position}, 0)` })
     g.classList.add(`node${division.nodeId}`)
 
-    makeSvg('line', { parent:g, x1:0, x2, y1:y, y2:y })
-    makeSvg('line', { parent:g, x1:0, x2:0, y1:y-4, y2:y+4, 'stroke-width':1 })
-    makeSvg('line', { parent:g, x1:x2, x2:x2, y1:y-4, y2:y+4, 'stroke-width':1 })
+    let lineMain = makeSvg('line',  { parent:g, x1:0, x2:width, y1:y, y2:y })
+    let lineStart = makeSvg('line', { parent:g, x1:0, x2:0, y1:y-4, y2:y+4, 'stroke-width':1 })
+    let lineEnd = makeSvg('line',   { parent:g, x1:width, x2:width, y1:y-4, y2:y+4, 'stroke-width':1 })
 
-    // division.on('move', () => {
-    //
-    //     let transform = `translate(${division.range.position * .5}, 0)`
-    //
-    //     makeSvg(g, { transform })
-    //
-    // })
+    const update = () => {
+
+        let position = division.range.position * .5
+        let width = division.range.width * .5
+
+        makeSvg(g, { transform:`translate(${position}, 0)` })
+        makeSvg(lineEnd, { x1:width, x2:width })
+
+    }
+
+    division.on('update', update)
 
 }
 
