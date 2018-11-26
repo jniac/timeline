76845123554b37de5d350ea940977b984ad9b055
jniac <joseph.merdrignac@gmail.com>
2018-11-26 14:02:24 +0100

    update test

--- test/weird-scroll/main.js
+++ test/weird-scroll/main.js
@@ -9,7 +9,8 @@ const withElement = (selector, callback) => [...document.querySelectorAll(select
 let timeline = new Timeline(window.innerHeight)
 
 // timeline.head.align = .5
-timeline.head.setProps({ translate:'-50%' })
+// timeline.head.setProps({ translate:'-50%' })
+timeline.head.setProps({ width:0 })
 
 let circle1 = makeCircle({ parent:document.querySelector('#part3 .content:first-child') })
 let circle2 = makeCircle({ parent:document.querySelector('#part3 .content:last-child') })
@@ -26,6 +27,8 @@ timeline.createDivision({
     document.querySelector('.main-content').style.transform = `translateY(${-x}px)`
 
 })
+
+timeline.createDivision({ name:'bound-min', parent:'scroll1', layout:'absolute', translate:'-100%', width:'100%' })
 
 timeline.createDivision({ name:'temporization-1', width:300 }).on('main-progress', ({ values }) => {
 
@@ -67,6 +70,9 @@ timeline.createDivision({
 
 })
 
+timeline.createDivision({ name:'bound-max', parent:'scroll3', layout:'absolute', position:'100%', width:'100%' })
+
+
 
 
 withElement('#part1,#part2', (element) => {
@@ -90,18 +96,17 @@ withElement('#part4,#part5', (element) => {
 
 
 timeline.update()
+timeline.rootContainer.setDirty()
 
 
 
-document.body.appendChild(new helpers.SvgTimelineHelper(timeline, { scale:1/24 }).svg)
+let svgHelper = new helpers.SvgTimelineHelper(timeline, { scale:1/24 })
+svgHelper.activeSpaceKey()
+document.body.appendChild(svgHelper.svg)
+
 new helpers.MouseDragHelper(timeline)
 new helpers.TouchDragHelper(timeline)
 new helpers.MouseWheelHelper(timeline, { direction:'biggestXorY' })
-
-
-
-timeline.forceUpdateHeads()
-timeline.forceUpdateHeads()
 
 
 
